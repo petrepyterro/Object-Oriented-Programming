ccbbc8dc320a99d11d86a713b4602356afa08fee
petrero <petrero@gmail.com>
2017-03-17 16:57:56 +0200

    Copy code from previous branch.

---
+++ play.php
@@ -0,0 +1,76 @@
+<?php
+class Ship{
+  public $name; 
+  
+  public $weaponPower = 0;
+  
+  public $jediFactor= 0;
+  
+  public $strength = 0;
+  
+  public function sayHello(){
+    echo 'HELLO!';
+  }
+  
+  public function getName() {
+    return $this->name;
+  }
+  
+  public function getNameAndSpecs($useShortFormat) {
+    if ($useShortFormat){
+      return sprintf(
+        '%s %s%s%s',
+        $this->name,
+        $this->weaponPower,
+        $this->jediFactor,
+        $this->strength      
+      );
+    } else {
+      return sprintf(
+        '%s (w:%s, j:%s, s:%s)',
+        $this->name,
+        $this->weaponPower,
+        $this->jediFactor,
+        $this->strength      
+      );
+    }
+  }
+  
+  public function doesGivenShipHaveMoreStrength($givenShip){
+    return $givenShip->strength > $this->strength;
+  }
+}
+
+function printShipSummary($someShip){
+  echo 'Ship Name: '.$someShip->name;
+  echo '<hr>';
+  echo $someShip->sayHello();
+  echo '<hr>';
+  echo $someShip->getName();  
+  echo '<hr>';
+  echo 'Ship Description: '. $someShip->getNameAndSpecs(false);
+  echo '<hr>';
+  echo 'Ship Description: '. $someShip->getNameAndSpecs(true);
+}
+
+$myShip=new Ship();
+$myShip->name = 'Jedi Starship';
+$myShip->weaponPower = 10;
+
+printShipSummary($myShip);
+
+$otherShip = new Ship();
+$otherShip->name = 'Imperial Shuttle';
+$otherShip->weaponPower = 5;
+$otherShip->strength = 50;
+
+echo '<hr/>';
+printShipSummary($otherShip);
+
+echo '<hr/>';
+if ($myShip->doesGivenShipHaveMoreStrength($otherShip)){
+  echo $otherShip->name . ' has more strength';
+} else {
+  echo $myShip->name . ' has more strength';
+}
+
