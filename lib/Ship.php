1107a2058ba1d3ab760e00d0da42cba9fbb2d579
petrero <petrero@gmail.com>
2017-03-17 19:24:31 +0200

    Copy code from previous branch

---
+++ lib/Ship.php
@@ -0,0 +1,77 @@
+<?php
+class Ship{
+  private $name;
+  private $weaponPower = 0;
+  private $jediFactor = 0;
+  private $strength = 0;
+  public function sayHello()
+  {
+    echo 'Hello!';
+  }
+  public function getName()
+  {
+    return $this->name;
+  }
+  
+  public function getWeaponPower()
+  {
+    return $this->weaponPower;
+  }
+  
+  public function getJediFactor()
+  {
+    return $this->jediFactor;
+  }
+  
+  public function setName($name)
+  {
+    $this->name = $name;
+  }
+  
+  public function setWeaponPower($weaponPower)
+  {
+    $this->weaponPower = $weaponPower;
+  }
+  
+  public function setJediFactor($jediFactor)
+  {
+    $this->jediFactor = $jediFactor;
+  }
+  
+  public function getNameAndSpecs($useShortFormat = false)
+  {
+    if ($useShortFormat) {
+      return sprintf(
+        '%s: %s/%s/%s',
+        $this->name,
+        $this->weaponPower,
+        $this->jediFactor,
+        $this->strength
+      );
+    } else {
+      return sprintf(
+        '%s: w:%s, j:%s, s:%s',
+        $this->name,
+        $this->weaponPower,
+        $this->jediFactor,
+        $this->strength
+      );
+    }
+  }
+  public function doesGivenShipHaveMoreStrength($givenShip)
+  {
+    return $givenShip->strength > $this->strength;
+  }
+  
+  public function setStrength($strength){
+    if(!is_numeric($strength)){
+      throw new Exception('Invalid strength passed '.$strength);
+    }
+    
+    $this->strength = $strength;
+  }
+  
+  public function getStrength(){
+    return $this->strength;
+  }
+}
